255a1950e75a6d9b8d59fb9b8e9f9cc04466fd5b
ksknght <kfefoncardaz@addu.edu.ph>
2024-08-25 20:41:51 +0800

    wip materials

--- Controllers/crudMaterials.js
+++ Controllers/crudMaterials.js
@@ -4,16 +4,37 @@ const prisma = new PrismaClient()
 
 // connect nlng daan ang supplier diri upon creation sa materials
 export const createMaterials = async (req, res) => {
-    
+    await prisma.materials.create({
+        data: {
+            supplier: {
+                connect: {
+                    id: req.body.supplierId,
+                }
+            },
+            name: req.body.matName
+        }
+    })
 }
 
 export const editMaterials = async (req, res) => {
-
+    await prisma.materials.update({
+        where: {
+            id: Number(req.params.id)
+        },
+        data: {
+            name: req.body.matName,
+            supplier: {
+                connect: {
+                    id: req.body.supplierId,
+                }
+            }
+        }
+    })
 }
 
 // for project overview
 export const removeMaterialsProject = async (req, res) => {
-
+    
 }
 
 // for project overview
